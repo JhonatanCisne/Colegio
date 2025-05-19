e86467141f6f4742e52d3765d6af3159dfc47819
Omarzb27 <omargomezcortez054@gmail.com>
2025-05-18 22:07:25 -0500

    Prueba

--- ColegioLaMerced/frontend/src/Paginas/Login/Login.js
+++ ColegioLaMerced/frontend/src/Paginas/Login/Login.js
@@ -9,7 +9,7 @@ function Login() {
 
       <div className="formulario-contenedor col-12 col-md-6 d-flex justify-content-center align-items-center">
         <form className="formulario-login w-75 p-4 rounded shadow">
-          <h2 className="text-center mb-4">Iniciar Sesión</h2>
+          <h2 className="text-center mb-4">asas</h2>
           <input type="text" placeholder="Usuario" className="form-control mb-3" />
           <input type="password" placeholder="Contraseña" className="form-control mb-3" />
           <button type="submit" className="btn btn-escarlata w-100">Entrar</button>
