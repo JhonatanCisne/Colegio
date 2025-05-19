de1c416d8eb72403d8738022e39fe292072359c3
Cisneros Jorge Jhonatan Eduardo <Jhonatanjorge151@gmail.com>
2025-05-18 21:14:06 -0500

    Login diseño

--- ColegioLaMerced/frontend/src/Paginas/Login/Login.js
+++ ColegioLaMerced/frontend/src/Paginas/Login/Login.js
@@ -1,26 +1,20 @@
-// src/Login.js
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './Login.css';  // Aquí puedes poner estilos personalizados
+import './Login.css';
 
 function Login() {
   return (
-    <div className="login-container d-flex justify-content-center align-items-center vh-100">
-      <form className="login-form p-4 shadow rounded bg-light">
-        <h2 className="mb-4 text-center">Iniciar sesión</h2>
-        
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label">Correo electrónico</label>
-          <input type="email" className="form-control" id="email" placeholder="usuario@ejemplo.com" />
-        </div>
+    <div className="login-contenedor d-flex vh-100">
+      <div className="imagen-login d-none d-md-block col-md-6"></div>
 
-        <div className="mb-3">
-          <label htmlFor="password" className="form-label">Contraseña</label>
-          <input type="password" className="form-control" id="password" placeholder="Contraseña" />
-        </div>
-
-        <button type="submit" className="btn btn-primary w-100">Entrar</button>
-      </form>
+      <div className="formulario-contenedor col-12 col-md-6 d-flex justify-content-center align-items-center">
+        <form className="formulario-login w-75 p-4 rounded shadow">
+          <h2 className="text-center mb-4">Iniciar Sesión</h2>
+          <input type="text" placeholder="Usuario" className="form-control mb-3" />
+          <input type="password" placeholder="Contraseña" className="form-control mb-3" />
+          <button type="submit" className="btn btn-escarlata w-100">Entrar</button>
+        </form>
+      </div>
     </div>
   );
 }
