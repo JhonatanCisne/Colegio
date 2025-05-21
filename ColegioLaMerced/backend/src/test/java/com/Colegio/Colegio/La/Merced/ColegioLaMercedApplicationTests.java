040bd334cf8e53e10dfea2aac30a2a8eb21eea83
Cisneros Jorge Jhonatan Eduardo <Jhonatanjorge151@gmail.com>
2025-05-20 21:22:16 -0500

    Creación de la carpeta con spring

---
+++ ColegioLaMerced/backend/src/test/java/com/Colegio/Colegio/La/Merced/ColegioLaMercedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Colegio.Colegio.La.Merced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColegioLaMercedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
